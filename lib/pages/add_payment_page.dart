c90db73f34fec458a617a1f66f3ced3cdb137071
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-31 18:58:32 +0500

    imp

--- lib/pages/add_payment_page.dart
+++ lib/pages/add_payment_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class AddPaymentPage extends StatefulWidget {
@@ -41,16 +44,154 @@ class _AddPaymentPageState extends State<AddPaymentPage> {
       backgroundColor: Color(0xff1D182A),
       appBar: AppBar(
         backgroundColor: Color(0xff1D182A),
+        leading: CupertinoButton(
+          onPressed: () {
+            Get.back();
+          },
+          child: Icon(
+            CupertinoIcons.chevron_back,
+            color: Colors.white,
+            size: 30,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Form(
+            key: key,
+            child: Column(
+              children: [
+                // Card Number
+                TextFormField(
+                  controller: cardController,
+                  focusNode: cardFocus,
+                  onTapOutside: (value) => cardFocus.unfocus(),
+                  inputFormatters: [cardMask],
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (cardMask.isFill() == false) {
+                      return "Karta kiritilsin";
+                    }
+                  },
+                  style: GoogleFonts.quicksand(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      filled: true,
+                      fillColor: Color(0xff342F3F),
+                      labelText: "Card Number",
+                      labelStyle: GoogleFonts.quicksand(
+                        color: Color(0xffFFFFFF).withOpacity(.5),
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18,
+                      )),
+                ),
+
+                // CVV & Exp
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: cvvController,
+                        focusNode: cvvFocus,
+                        onTapOutside: (value) => cvvFocus.unfocus(),
+                        inputFormatters: [cvvMask],
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (cvvMask.isFill() == false) {
+                            return "CVV Kiritilsin";
+                          }
+                        },
+                        style: GoogleFonts.quicksand(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                        ),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          filled: true,
+                          fillColor: Color(0xff342F3F),
+                          labelText: "CVV",
+                          labelStyle: GoogleFonts.quicksand(
+                            color: Color(0xffFFFFFF).withOpacity(.5),
+                            fontWeight: FontWeight.w500,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Expanded(
+                      child: TextFormField(
+                        controller: expController,
+                        focusNode: expFocus,
+                        onTapOutside: (value) => expFocus.unfocus(),
+                        inputFormatters: [expMask],
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (expMask.isFill() == false) {
+                            return "Exp kiritilsin";
+                          }
+                        },
+                        style: GoogleFonts.quicksand(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                        ),
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            filled: true,
+                            fillColor: Color(0xff342F3F),
+                            labelText: "Exp",
+                            labelStyle: GoogleFonts.quicksand(
+                              color: Color(0xffFFFFFF).withOpacity(.5),
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18,
+                            )),
+                      ),
+                    ),
+                  ],
+                ),
+
+                // Name
+                Spacer(),
+                CupertinoButton(
+                  color: Color(0xff8E6CEF),
+                  onPressed: () async {
+                    if (key.currentState!.validate()) {
+                      // Create...
+                      Get.back();
+                    }
+                  },
+                  child: Center(
+                    child: Text(
+                      "Save",
+                      style: GoogleFonts.quicksand(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
