c511fba03a058df7e7aad7ac86cfa09b16a31186
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-31 17:16:28 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/azimislom/d5_8_12/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.azimislom.d5_8_12
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/add_payment_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddPaymentPage extends StatefulWidget {
+  const AddPaymentPage({super.key});
+
+  @override
+  State<AddPaymentPage> createState() => _AddPaymentPageState();
+}
+
+class _AddPaymentPageState extends State<AddPaymentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
