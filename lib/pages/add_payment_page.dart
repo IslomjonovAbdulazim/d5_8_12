e1f887ded1c9dfa2f2b5e7ebe6b5089a1f8ca354
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-31 18:36:18 +0500

    imp

--- lib/pages/add_payment_page.dart
+++ lib/pages/add_payment_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class AddPaymentPage extends StatefulWidget {
   const AddPaymentPage({super.key});
@@ -8,8 +9,48 @@ class AddPaymentPage extends StatefulWidget {
 }
 
 class _AddPaymentPageState extends State<AddPaymentPage> {
+  final cardController = TextEditingController();
+  final cardFocus = FocusNode();
+  final cardMask = MaskTextInputFormatter(
+    mask: "#### #### #### ####",
+    filter: {"#": RegExp(r'[0-9]')},
+  );
+
+  final cvvController = TextEditingController();
+  final cvvFocus = FocusNode();
+  final cvvMask = MaskTextInputFormatter(
+    mask: "###",
+    filter: {"#": RegExp(r'[0-9]')},
+  );
+
+  final expController = TextEditingController();
+  final expFocus = FocusNode();
+  final expMask = MaskTextInputFormatter(
+    mask: "##/##",
+    filter: {"#": RegExp(r'[0-9]')},
+  );
+
+  final nameController = TextEditingController();
+  final nameFocus = FocusNode();
+
+  final key = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xff1D182A),
+      appBar: AppBar(
+        backgroundColor: Color(0xff1D182A),
+      ),
+    );
   }
 }
+
+
+
+
+
+
+
+
+
