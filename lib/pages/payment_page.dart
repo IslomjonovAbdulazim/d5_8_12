a96f4c2cf67b4366d1085f887bd44a3f56548ac6
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-31 17:41:42 +0500

    imp

--- lib/pages/payment_page.dart
+++ lib/pages/payment_page.dart
@@ -13,3 +13,44 @@ class _PaymentPageState extends State<PaymentPage> {
     return Scaffold();
   }
 }
+
+class PaymentModel {
+  // members
+  late int cardNumber;
+  late int cvv;
+  late String exp;
+  late String cardholderName;
+
+  // constructor
+  PaymentModel({
+    required this.cardNumber,
+    required this.cvv,
+    required this.exp,
+    required this.cardholderName,
+  });
+
+  // fromJson
+  PaymentModel.fromJson(Map json) {
+    cardNumber = json["cardNumber"];
+    cvv = json["cvv"];
+    exp = json["exp"];
+    cardholderName = json["cardholderName"];
+  }
+
+  // toJson
+  Map toJson() => {
+    "cardNumber": cardNumber,
+    "cvv": cvv,
+    "exp": exp,
+    "cardholderName": cardholderName,
+  };
+}
+
+
+
+
+
+
+
+
+
